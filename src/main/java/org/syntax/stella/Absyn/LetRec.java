3078ff71803d5042b151f52fbc677354c2afe969
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2023-03-30 13:10:20 +0300

    Update grammar and AST

--- src/main/java/org/syntax/stella/Absyn/LetRec.java
+++ src/main/java/org/syntax/stella/Absyn/LetRec.java
@@ -2,18 +2,18 @@
 
 package org.syntax.stella.Absyn;
 
-public class Let  extends Expr {
+public class LetRec  extends Expr {
   public final ListPatternBinding listpatternbinding_;
   public final Expr expr_;
   public int line_num, col_num, offset;
-  public Let(ListPatternBinding p1, Expr p2) { listpatternbinding_ = p1; expr_ = p2; }
+  public LetRec(ListPatternBinding p1, Expr p2) { listpatternbinding_ = p1; expr_ = p2; }
 
   public <R,A> R accept(org.syntax.stella.Absyn.Expr.Visitor<R,A> v, A arg) { return v.visit(this, arg); }
 
   public boolean equals(java.lang.Object o) {
     if (this == o) return true;
-    if (o instanceof org.syntax.stella.Absyn.Let) {
-      org.syntax.stella.Absyn.Let x = (org.syntax.stella.Absyn.Let)o;
+    if (o instanceof org.syntax.stella.Absyn.LetRec) {
+      org.syntax.stella.Absyn.LetRec x = (org.syntax.stella.Absyn.LetRec)o;
       return this.listpatternbinding_.equals(x.listpatternbinding_) && this.expr_.equals(x.expr_);
     }
     return false;
